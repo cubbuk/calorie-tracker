7ce646f25d96ff65c70edb9888aaea006b8093ae
Mehmet Çubuk <mehmetcubuk@20satir.com>
2016-10-27 18:43:58 +0300

    basic rest test

--- backend/tests/rest/users/users_route_tests.js
+++ backend/tests/rest/users/users_route_tests.js
@@ -1,24 +1,24 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require("../../../server");
-let should = chai.should();
-chai.use(chaiHttp);
+// const chai = require('chai');
+// const chaiHttp = require('chai-http');
+// const server = require("../../../server");
+// let should = chai.should();
+// chai.use(chaiHttp);
+//
+// const expect = chai.expect;
+// const usersService = require("../../../components/users/_services/users_service");
+// const mongoose = require("mongoose");
 
-const expect = chai.expect;
-const usersService = require("../../../components/users/_services/users_service");
-const mongoose = require("mongoose");
 
 
+// describe("Users Route tests", function () {
 
-describe("Users Route tests", function () {
+    // it("return 401 without authorization", function (done) {
+    //     chai.request(server)
+    //         .post("/api/users/search", {})
+    //         .end(function(err, res){
+    //             res.should.have.status(401);
+    //             done();
+    //         });
+    // });
 
-    it("return 401 without authorization", function (done) {
-        chai.request(server)
-            .post("/api/users/search", {})
-            .end(function(err, res){
-                res.should.have.status(401);
-                done();
-            });
-    });
-
-});
+// });
