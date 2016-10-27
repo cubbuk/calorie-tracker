002b4eb73c2591eb512b7709b45063c20a279ef6
Mehmet Çubuk <mehmetcubuk@20satir.com>
2016-10-27 17:22:05 +0300

    test utility for initialization of data

--- backend/utility/test_utility.js
+++ backend/utility/test_utility.js
@@ -2,12 +2,13 @@ const Promise = require("bluebird");
 const mongoose = require("mongoose");
 class TestUtility {
     constructor(){
-        this.conn = mongoose.connect("mongodb://localhost/calorie_tracker_db");
+        this.conn = mongoose.connect("mongodb://localhost/calorie_tracker_test_db");
     }
     dropTestDatabase() {
         return new Promise((resolve, reject) => {
             this.conn.connection.dropDatabase((err) => {
                 if (err) {
+                    console.log(err);
                     reject(err);
                 } else {
                     resolve();
